39ee13d701c7fd2fc0f0afa8b6f04f439275994a
William Clifford <wobh@yahoo.com>
2013-04-24 14:22:45 -0700

    setting up the jungle

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,35 @@
 
+var Jungle = $(function(){
+    var Beast = Backbone.Model.extend({
+	urlRoot: '/beast'
+    });
+
+    var Beasts = Backbone.Collection.extend({
+	url: '/beasts',
+	model: Beast
+    });
+				       
+    var BeastsView = Backbone.View.extend({
+	el: '#beasts',
+
+	initialize: function () {
+	    this.collection.fetch({reset: true});
+	},
+
+	render: function () {
+	    this.collection.each( function(beast) {
+		this.$el.append (beast);
+		}, this);
+	    return this;
+	},
+
+	addOne: function (beast) { 
+	},
+
+	addAll: function (beasts) {
+	}
+    });
+
+
+    return new BeastsView();
+})();
